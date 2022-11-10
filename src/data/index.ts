9a18df657d242ef2e86c01fdda72c31f015c3b42
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-11-10 16:49:32 +0000

    Remove data

--- src/data/index.ts
+++
@@ -1,7 +0,0 @@
-import modules from '../data/modules.json';
-import populatedSystems from '../data/systems_populated.json';
-import outfittingStations from '../data/stations_with_outfitting.json';
-
-const data = { modules, populatedSystems, outfittingStations };
-
-export default data;
